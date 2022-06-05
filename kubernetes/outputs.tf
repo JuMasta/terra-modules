8a4219dd4ac40d8ec18ab5828e5948e59fac2803
Nikita Bulatov <43339735+JuMasta@users.noreply.github.com>
2022-06-05 20:20:02 +0200

    Update outputs.tf

--- kubernetes/outputs.tf
+++ kubernetes/outputs.tf
@@ -11,3 +11,8 @@ output "kube_admin_config" {
   value = azurerm_kubernetes_cluster.cluster.kube_admin_config
   # sensitive = true
 }
+
+output "loadbalancer_id" {
+  value = azurerm_kubernetes_cluster.cluster.network_profile.load_balancer_profile.id
+}
+
